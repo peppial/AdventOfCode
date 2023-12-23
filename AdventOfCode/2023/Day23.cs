f0581ed6b95c5c99aeda99d62d0e1d3234f3b61a
Penka Alexandrova <penka.alexandrova@scaleforce.io>
2023-12-23 21:21:27 +0200

    Added Day 23

---
+++ AdventOfCode/2023/Day23.cs
@@ -0,0 +1,100 @@
+using Advent2023.Structures;
+   
+   namespace Advent2023;
+
+   public class Day23(string[] lines) : IDay
+   {
+       int len = lines[0].Length;
+       int height = lines.Length;
+
+       public long GetTotalPartA()
+       {
+           return Solve(true);
+       }
+
+
+       public long GetTotalPartB()
+       {
+           return Solve(false);
+       }
+
+       private long Solve(bool partA)
+       {
+           int startIndex = lines[0].IndexOf('.');
+           Stack<(Coordinate2D, HashSet<Coordinate2D>, int steps)> queue = new();
+           queue.Push(((0, startIndex), [(0, startIndex)], 0));
+           Dictionary<Coordinate2D, bool> all = [];
+
+           HashSet<(int, int, int)> seen = [];
+           foreach (int i in height - 1)
+           {
+               foreach (int j in (len - 1)) all[(i, j)] = (lines[i][j] != '#');
+           }
+
+           int maxSteps = 0;
+           while (queue.Any())
+           {
+               (Coordinate2D start, HashSet<Coordinate2D> visited, int steps) = queue.Pop();
+
+               if (steps > 7000) continue;
+               //Console.WriteLine(start);
+
+               char symbol = lines[start.X][start.Y];
+
+               if (start.X == len - 1)
+               {
+                   if (steps > maxSteps)
+                   {
+                       maxSteps = steps;
+                       Console.WriteLine(maxSteps);
+                   }
+
+                   continue;
+               }
+
+               if (partA && symbol is '.' or '<' || !partA)
+               {
+                   if (start.Y > 0 && all[(start.X, start.Y - 1)] && !visited.Contains((start.X, start.Y - 1)))
+                   {
+                       var v = new HashSet<Coordinate2D>(visited);
+                       v.Add((start.X, start.Y - 1));
+                       queue.Push(((start.X, start.Y - 1), v, steps + 1));
+                   }
+               }
+
+               if (partA && symbol is '.' or '^'|| !partA)
+               {
+                   if (start.X > 0 && all[(start.X - 1, start.Y)] && !visited.Contains((start.X - 1, start.Y)))
+                   {
+                       var v = new HashSet<Coordinate2D>(visited);
+                       v.Add((start.X - 1, start.Y));
+                       queue.Push(((start.X - 1, start.Y), v, steps + 1));
+                   }
+               }
+               if (partA && symbol is '.' or 'v' || !partA)
+               {
+                   if (start.X < len - 1 && all[(start.X + 1, start.Y)] && !visited.Contains((start.X + 1, start.Y)))
+                   {
+                       var v = new HashSet<Coordinate2D>(visited);
+                       v.Add((start.X + 1, start.Y));
+                       queue.Push(((start.X + 1, start.Y), v, steps + 1));
+
+                   }
+               }
+               if (partA && symbol is '.' or '>'|| !partA)
+               {
+                   if (start.Y < height - 1 && all[(start.X, start.Y + 1)] && !visited.Contains((start.X, start.Y + 1)))
+                   {
+                       var v = new HashSet<Coordinate2D>(visited);
+                       v.Add((start.X, start.Y + 1));
+                       queue.Push(((start.X, start.Y + 1), v, steps + 1));
+                   }
+               }
+        
+           }
+
+           return maxSteps;
+       }
+   }
+   
+   
--- AdventOfCode/2023/Program.cs
+++ AdventOfCode/2023/Program.cs
@@ -5,8 +5,6 @@ using Advent2023.Structures;
 
 Console.WriteLine(DateTime.Now.ToString("hh:mm:ss.fff tt")) ;
 var lines = File.ReadAllLines("Test.txt");
-var day = new Day22(lines);
+var day = new Day23(lines);
 Console.WriteLine("-----------");
-Console.WriteLine(day.GetTotalPartB());
-Coordinate3D c = new Coordinate3D("1,2,3");
-Console.WriteLine(DateTime.Now.ToString("hh:mm:ss.fff tt")) ;
+Console.WriteLine(day.GetTotalPartA());
