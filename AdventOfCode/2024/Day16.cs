b8082479a63da0e163a78708f6d8f68794fc2919
Penka Alexandrova <info@ermena.com>
2024-12-16 21:32:42 +0200

    Reformat

--- AdventOfCode/2024/Day16.cs
+++ AdventOfCode/2024/Day16.cs
@@ -43,18 +43,21 @@ public class Day16: IDay
     {
         return GetResult(false);
     }
+
     long GetResult(bool partA)
     {
         Direction direction = Direction.Right;
-        
-        queue.Enqueue((SPos.row, SPos.col,Direction.Right,0,[(SPos.row, SPos.col)],$"({SPos.row}, {SPos.col})"),0);
-        queue.Enqueue((SPos.row, SPos.col,Direction.Down,1000,[(SPos.row, SPos.col)], $"({SPos.row}, {SPos.col})"),1000);
-        queue.Enqueue((SPos.row, SPos.col,Direction.Up,1000,[(SPos.row, SPos.col)],$"({SPos.row}, {SPos.col})"),1000);
-        
+
+        queue.Enqueue((SPos.row, SPos.col, Direction.Right, 0, [(SPos.row, SPos.col)], $"({SPos.row}, {SPos.col})"), 0);
+        queue.Enqueue((SPos.row, SPos.col, Direction.Down, 1000, [(SPos.row, SPos.col)], $"({SPos.row}, {SPos.col})"),
+            1000);
+        queue.Enqueue((SPos.row, SPos.col, Direction.Up, 1000, [(SPos.row, SPos.col)], $"({SPos.row}, {SPos.col})"),
+            1000);
+
         while (queue.Count > 0)
         {
             var pos = queue.Dequeue();
-            
+
             direction = pos.direction;
             (int row, int col) pos1 = direction switch
             {
@@ -63,25 +66,27 @@ public class Day16: IDay
                 Direction.Left => (pos.row, pos.col - 1),
                 Direction.Up => (pos.row - 1, pos.col)
             };
-     
-            int nextscore=pos.score + 1;
+
+            int nextscore = pos.score + 1;
             if (IsInGrid(pos1))
             {
                 if (lines[pos1.row][pos1.col] == 'E')
                 {
-                    
+
                     Console.WriteLine(pos.path);
                     if (partA) return nextscore;
-                    if(nextscore>bestScore) break;
-                    foreach(var s in pos.set) finalSet.Add(s);
+                    if (nextscore > bestScore) break;
+                    foreach (var s in pos.set) finalSet.Add(s);
                     bestScore = nextscore;
                 }
-               
+
                 if (lines[pos1.row][pos1.col] == '.')
                 {
-                    AddToQueue(pos1.row, pos1.col, direction, nextscore,pos.set, pos.path + $"({pos1.row}, {pos1.col})");
+                    AddToQueue(pos1.row, pos1.col, direction, nextscore, pos.set,
+                        pos.path + $"({pos1.row}, {pos1.col})");
                 }
             }
+
             nextscore = pos.score + 1001;
             (int row, int col) pos2 = direction switch
             {
@@ -92,48 +97,50 @@ public class Day16: IDay
             };
             if (IsInGrid(pos2))
             {
-                if (lines[pos2.row][pos2.col] == 'E' )
+                if (lines[pos2.row][pos2.col] == 'E')
                 {
                     Console.WriteLine(pos.path);
                     if (partA) return nextscore;
-                    if(nextscore>bestScore) break;
-                    foreach(var s in pos.set) finalSet.Add(s);
+                    if (nextscore > bestScore) break;
+                    foreach (var s in pos.set) finalSet.Add(s);
                     bestScore = nextscore;
                 }
-                
+
                 if (lines[pos2.row][pos2.col] == '.')
-                {   
-                    AddToQueue(pos2.row, pos2.col, IncrementEnum(direction), nextscore, pos.set,pos.path + $"({pos2.row}, {pos2.col})");
+                {
+                    AddToQueue(pos2.row, pos2.col, IncrementEnum(direction), nextscore, pos.set,
+                        pos.path + $"({pos2.row}, {pos2.col})");
                 }
             }
-            
+
             (int row, int col) pos3 = direction switch
             {
-                Direction.Right => (pos.row-1, pos.col),
-                Direction.Down => (pos.row, pos.col+1),
-                Direction.Left => (pos.row+1, pos.col),
-                Direction.Up => (pos.row, pos.col-1)
+                Direction.Right => (pos.row - 1, pos.col),
+                Direction.Down => (pos.row, pos.col + 1),
+                Direction.Left => (pos.row + 1, pos.col),
+                Direction.Up => (pos.row, pos.col - 1)
             };
-            
+
             if (IsInGrid(pos3))
             {
                 if (lines[pos3.row][pos3.col] == 'E')
                 {
                     Console.WriteLine(pos.path);
                     if (partA) return nextscore;
-                    if(nextscore>bestScore) break;
-                    foreach(var s in pos.set) finalSet.Add(s);
+                    if (nextscore > bestScore) break;
+                    foreach (var s in pos.set) finalSet.Add(s);
                     bestScore = nextscore;
                 }
 
                 if (lines[pos3.row][pos3.col] == '.')
                 {
-                    AddToQueue(pos3.row, pos3.col, DecrementEnum(direction), nextscore, pos.set,pos.path + $"({pos3.row}, {pos3.col})");
+                    AddToQueue(pos3.row, pos3.col, DecrementEnum(direction), nextscore, pos.set,
+                        pos.path + $"({pos3.row}, {pos3.col})");
                 }
             }
         }
-        
-        return finalSet.Count+1;
+
+        return finalSet.Count + 1;
 
         void AddToQueue(int row, int col, Direction direction, int score, HashSet<(int row, int col)> set, string path)
         {
@@ -146,8 +153,9 @@ public class Day16: IDay
             lowestCost[key] = score;
             HashSet<(int row, int col)> nset = new(set);
             nset.Add((row, col));
-            queue.Enqueue((row, col, direction, score,nset, path + $"({row}, {col})"), score);
+            queue.Enqueue((row, col, direction, score, nset, path + $"({row}, {col})"), score);
         }
+
         bool IsInGrid((int row, int col) pos)
         {
             return (pos.row > 0 && pos.row < rows && pos.col > 0 && pos.col < cols);
