a6c0489bfe97a7f28528672e2d954c9d57a6d6a6
Penka Alexandrova <info@ermena.com>
2025-12-12 08:47:05 +0200

    2025 Day 12

---
+++ AdventOfCode/2025/Day12.cs
@@ -0,0 +1,27 @@
+using AdventOfCode.Extensions;
+
+namespace AdventOfCode._2025;
+
+public class Day12(string[] lines) : IDay
+{
+    public long GetTotalPartA() => Calculate(false);
+
+    public long GetTotalPartB() => Calculate(true);
+
+
+    private long Calculate(bool partB)
+    {
+        int count = 0;
+        for (int i = 30; i < lines.Length; i++)
+        {
+            var numbers = lines[i].GetNumbers();
+            var size = numbers[0] * numbers[1];
+            long sum = numbers[2..].Sum() * 8;
+            if (size > sum) count++;
+
+        }
+
+        return count;
+    }
+
+}
